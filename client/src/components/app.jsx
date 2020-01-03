b0203c5e514f447350606e3910a48ae8d2752706
AdamMomen <adammuman81@gmail.com>
2020-01-03 17:26:05 +0100

    i can do it

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -4,12 +4,47 @@ var io = require('socket.io-client')
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+
+        this.state = {
+            message: '',
+            messages: null,
+        }
     }
-    componentDidMount() {
-        var socket = io();
-        socket.on('message', function (data) {
+    componentWillUpdate() {
+        // var socket = io();
+        socket.on('message', (data) => {
             //set the state though
-            console.log(data);
+            this.setState({
+                messages: data
+            });
+        })
+        console.log('from socket', this.state.messages)
+    }
+
+    onChange(e) {
+        this.setState({
+            message: e.target.value,
+        })
+
+    }
+    handleSubmit() {
+        console.log(this.state.message)
+        if (this.state.message) {
+
+            var message = {
+                message: this.state.message
+            }
+
+            fetch('http://localhost:3000/message', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(message)
+            }).then(console.log(`Sent successfully!`))
+        }
+        this.setState({
+            message: ''
         })
     }
     render() {
@@ -18,10 +53,10 @@ class App extends React.Component {
                 <div id="message-conntainer" style={{ height: "500px", width: "200px", border: "1px solid grey", "borderRadius": "10px" }}>
                     Messages should be rendered here!!
                 </div>
-                <form id="message-form">
-                    <input type="text" />
-                    <input type="submit" />
-                </form>
+                {/* <form id="message-form" onSubmit={this.handleClick}> */}
+                <input type="text" onChange={this.onChange} value={this.state.message}></input>
+                <button onClick={this.handleSubmit}>Submit</button>
+                {/* </form> */}
             </div >
         )
     }
