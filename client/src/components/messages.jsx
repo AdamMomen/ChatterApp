dc95419bedf47e312fc2f1e398c5d98244888f96
AdamMomen <adammuman81@gmail.com>
2020-01-03 21:17:29 +0100

    create factories

---
+++ client/src/components/messages.jsx
@@ -0,0 +1,77 @@
+import React, { useRef } from "react"
+import ReactDOM from 'react-dom'
+import { USER_CONNECTED } from '../events'
+
+// import { useEffect, useRef } from "react"
+// import io from 'socket.io-client'
+var io = require('socket.io-client')
+var socketURL = 'http://localhost:3000'
+
+class Messages extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: '',
+            messages: null,
+            socket: null,
+            user: null,
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.onChange = this.onChange.bind(this)
+    }
+
+    componentDidMount() {
+        var socket = io().on('connect', () => {
+            console.log('connected!')
+        })
+        this.setState({ socket })
+        console.log(socket)
+    }
+    setUser(user) {
+        const { socket } = this.state
+        socket.emit(USER_CONNECTED, user)
+        this.setState({ user })
+    }
+    logout() {
+        const { socket } = this.state
+        socket.emit(LOGOUT)
+        this.setState({ user: null })
+    }
+    onChange(e) {
+        this.setState({
+            message: e.target.value,
+        })
+
+    }
+    handleSubmit() {
+        console.log(this.state.message)
+        if (this.state.message) {
+
+            var message = {
+                message: this.state.message
+            }
+            fetch('http://localhost:3000/message', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(message)
+            }).then(console.log(`Sent successfully!`))
+        }
+        this.setState({
+            message: ''
+        })
+    }
+    render() {
+
+        return (
+            <div> <div>
+                <div id="message-conntainer" style={{ height: "500px", width: "200px", border: "1px solid grey", "borderRadius": "10px" }}>
+                    Messages should be rendered here!!
+            </div>
+                <input type="text" onChange={this.onChange} value={this.state.message}></input>
+                <button onClick={this.handleSubmit}>Submit</button>
+            </div >
+            </div>
+        )
+    }
+}
+export default Messages
---
+++ client/src/factories.js
@@ -0,0 +1,9 @@
+const uuidv4 = require('uuid/v4')
+const createUser = ({ name: '' } = {}) => (
+    {
+        id: uuidv4(),
+        name
+    }
+)
+
+//create
