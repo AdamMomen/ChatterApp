87b9a7791d566e2d912a03bcb4abe7814d314134
AdamMomen <adammuman81@gmail.com>
2020-01-03 10:40:36 +0100

    server running

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static(''))
+app.use(express.static('client/dist'))
 var port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
