b28d45ba23b6f691c38dbfa8552ead4905d19953
AdamMomen <adammuman81@gmail.com>
2020-01-03 11:14:25 +0100

    added Babel config file

---
+++ client/src/components/app.jsx
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    constructor(props) {
+    }
+    render() {
+        return (
+            <div>
+                Hello from REACTs
+                </div>
+        )
+    }
+}
+ReactDOM.render(<App />, document.getElementById('app'))
