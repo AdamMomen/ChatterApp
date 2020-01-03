ac18d93ffaeffb0e2cfe1ba4ba504d2e544b47bf
AdamMomen <adammuman81@gmail.com>
2020-01-03 19:37:49 +0100

    added the db config setup

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -7,8 +7,8 @@ var io = require('socket.io-client')
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.onChange = this.onChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+        // this.onChange = this.onChange.bind(this);
 
         this.state = {
             message: '',
@@ -26,34 +26,38 @@ class App extends React.Component {
         // console.log('from socket', this.state.messages)
     }
 
-    onChange(e) {
-        this.setState({
-            message: e.target.value,
-        })
+    // onChange(e) {
+    //     this.setState({
+    //         message: e.target.value,
+    //     })
 
-    }
-    handleSubmit() {
-        console.log(this.state.message)
-        if (this.state.message) {
+    // }
+    // handleSubmit() {
+    //     console.log(this.state.message)
+    //     if (this.state.message) {
 
-            var message = {
-                message: this.state.message
-            }
-            $.post('http://localhost:3000/message', JSON.stringify(message))
-            this.setState({
-                message: ''
-            })
-        }
-    }
+    //         var message = {
+    //             message: this.state.message
+    //         }
+    //         fetch('http://localhost:3000/message', {
+    //             method: "POST",
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify(message)
+    //         }).then(console.log(`Sent successfully!`))
+    //     }
+    //     this.setState({
+    //         message: ''
+    //     })
+    // }
     render() {
-        return (
-            <div>
-                <div id="message-conntainer" style={{ height: "500px", width: "200px", border: "1px solid grey", "borderRadius": "10px" }}>
-                    Messages should be rendered here!!
+        return (<div>
+            {/* <div id="message-conntainer" style={{ height: "500px", width: "200px", border: "1px solid grey", "borderRadius": "10px" }}>
+                Messages should be rendered here!!
                 </div>
-                <input type="text" onChange={this.onChange} value={this.state.message}></input>
-                <button onClick={this.handleSubmit}>Submit</button>
-            </div >
+            <input type="text" onChange={this.onChange} value={this.state.message}></input>
+            <button onClick={this.handleSubmit}>Submit</button> */}
+            <Messages />
+        </div >
             // <Messages
         )
     }
