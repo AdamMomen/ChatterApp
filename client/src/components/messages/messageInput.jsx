25f9bbd02acfdd6fa3a25d4baac958ca2587dce1
AdamMomen <adammuman81@gmail.com>
2020-01-04 03:58:23 +0100

    I need to sleeep

---
+++ client/src/components/messages/messageInput.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+class MessageInput extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>MessageInput</div>
+        )
+    }
+}
+export default MessageInput
